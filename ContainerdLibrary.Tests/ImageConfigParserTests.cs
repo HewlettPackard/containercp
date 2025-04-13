66dba4d4a3a2835521d903dbd4e0d4867f5f9d6e
tal.aloni <tal.aloni@hpe.com>
2025-04-13 22:49:28 +0300

    Add ImageConfigParser

---
+++ ContainerdLibrary.Tests/ImageConfigParserTests.cs
@@ -0,0 +1,31 @@
+﻿/* Copyright 2023 Hewlett Packard Enterprise Development LP.
+ * 
+ * You can redistribute this program and/or modify it under the terms of
+ * the GNU Lesser Public License version 2.1
+ */
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ContainerdLibrary.Tests
+{
+    [TestClass]
+    public class ImageConfigParserTests
+    {
+        [TestMethod]
+        [DeploymentItem(@"TestData\io.containerd.content.v1.content\blobs\sha256\7c2d729072a7ad556cb0ac2f53bfb5b1ab6dd511d1b48be9f009c9166bde6ee3", @"TestData\io.containerd.content.v1.content\blobs\sha256\7c2d729072a7ad556cb0ac2f53bfb5b1ab6dd511d1b48be9f009c9166bde6ee3")]
+        public void Test_GetLayersHashList()
+        {
+            // Arrange
+            string manifestPath = @"TestData\io.containerd.content.v1.content\blobs\sha256\7c2d729072a7ad556cb0ac2f53bfb5b1ab6dd511d1b48be9f009c9166bde6ee3";
+            string manifest = File.ReadAllText(manifestPath);
+
+            // Act
+            List<string> diffIDs = ImageConfigParser.GetImageDiffIDs(manifest);
+
+            // Assert
+            Assert.AreEqual(4, diffIDs.Count);
+            Assert.AreEqual("sha256:fffe76c64ef2dee2d80a8bb3ad13d65d596d04a45510b1956a976a69215dae92", diffIDs[0]);
+        }
+    }
+}
---
+++ ContainerdLibrary/ImageConfigParser.cs
@@ -0,0 +1,31 @@
+﻿/* Copyright 2023-2025 Hewlett Packard Enterprise Development LP.
+ * 
+ * You can redistribute this program and/or modify it under the terms of
+ * the GNU Lesser Public License version 2.1
+ */
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace ContainerdLibrary
+{
+    public class ImageConfigParser
+    {
+        private const string RootFsPropertyName = "rootfs";
+        private const string DiffIDsPropertyName = "diff_ids";
+
+        public static List<string> GetImageDiffIDs(string configJson)
+        {
+            List<string> result = new List<string>();
+            JObject config = JObject.Parse(configJson);
+            JObject rootFs = config.GetValue(RootFsPropertyName) as JObject;
+            JArray entries = rootFs.GetValue(DiffIDsPropertyName) as JArray;
+            foreach (JValue entry in entries)
+            {
+                string hash = entry.Value<string>();
+                result.Add(hash);
+            }
+
+            return result;
+        }
+    }
+}
